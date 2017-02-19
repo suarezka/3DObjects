c7c21566265abb910df018e5e7d3590f4c3d0e17
natebenson32 <nathanbenson32@gmail.com>
2017-02-19 04:03:01 -0500

    FINISHED THE SPHERE

--- Sphere.js
+++ Sphere.js
@@ -24,11 +24,11 @@ class Sphere {
          in the following loop we pack both position and color
          so each tuple (x,y,z,r,g,b) describes the properties of a vertex
          */
-        vertices.push(0,0,radius); /* tip of cone */
+        vertices.push(0,0,-radius); /* tip of cone */
         vec3.lerp (randColor, col1, col2, Math.random()); /* linear interpolation between two colors */
         vertices.push(randColor[0], randColor[1], randColor[2]);
-        for (let i = (stacks * 2) - 1; i >= 0; i--) {
-            let anglePhi =  i * Math.PI / (stacks * 2);
+        for (let i = stacks - 1; i >= 0; i--) {
+            let anglePhi =  i * Math.PI / stacks;
             for (let k = 0; k < subDiv; k++) {
                 let angle = k * 2 * Math.PI / subDiv;
                 let x = radius * Math.cos(angle) * Math.sin(anglePhi);
@@ -53,30 +53,48 @@ class Sphere {
         gl.bindBuffer(gl.ARRAY_BUFFER, this.vbuff);
         gl.bufferData(gl.ARRAY_BUFFER, Float32Array.from(vertices), gl.STATIC_DRAW);
 
-        // Generate index order for top of cone
+        // Generate index order for top of sphere
         let topIndex = [];
-        //topIndex.push(0);
-        for (let k = 0; k < (subDiv * 8); k++)
+        topIndex.push(0);
+        for (let k = 1; k <= subDiv; k++)
             topIndex.push(k);
         topIndex.push(1);
         this.topIdxBuff = gl.createBuffer();
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.topIdxBuff);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, Uint8Array.from(topIndex), gl.STATIC_DRAW);
 
-        // // Generate index order for bottom of cone
-        // let botIndex = [];
-        // botIndex.push(subDiv + 1);
-        // for (let k = subDiv; k >= 1; k--)
-        //     botIndex.push(k);
-        // botIndex.push(subDiv);
-        // this.botIdxBuff = gl.createBuffer();
-        // gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.botIdxBuff);
-        // gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, Uint8Array.from(botIndex), gl.STATIC_DRAW);
+        // Generate index order for middle of sphere
+        let middleIndex = [];
+        for (let k = 1; k <= subDiv * stacks - (subDiv * 2); k++) {
+            middleIndex.push(k);
+            middleIndex.push(k + subDiv);
+
+            if (k % subDiv == 0) {
+                middleIndex.push(k - (subDiv - 1));
+                middleIndex.push(k + 1);
+            }
+        }
+        this.midIdxBuff = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.midIdxBuff);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, Uint8Array.from(middleIndex), gl.STATIC_DRAW);
+        console.log(vertices.length / 6);
+
+        // Generate index order for bottom of sphere
+        let botIndex = [];
+        let max = subDiv * stacks - subDiv + 2;
+        botIndex.push(max);
+        for (let k = max - 1; k >= (max - 1) - subDiv; k--)
+            botIndex.push(k);
+        botIndex.push(max - 2);
+        this.botIdxBuff = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.botIdxBuff);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, Uint8Array.from(botIndex), gl.STATIC_DRAW);
 
         /* Put the indices as an array of objects. Each object has three attributes:
          primitive, buffer, and numPoints */
-        this.indices = [{"primitive": gl.POINT, "buffer": this.topIdxBuff, "numPoints": topIndex.length}];
-        //{"primitive": gl.POINT, "buffer": this.botIdxBuff, "numPoints": botIndex.length}];
+        this.indices = [{"primitive": gl.TRIANGLE_FAN, "buffer": this.topIdxBuff, "numPoints": topIndex.length},
+            {"primitive": gl.TRIANGLE_STRIP, "buffer": this.midIdxBuff, "numPoints": middleIndex.length},
+            {"primitive": gl.TRIANGLE_FAN, "buffer": this.botIdxBuff, "numPoints": botIndex.length}];
     }
 
     /**
